68c17140b77c8656f0613ad21f94c7c42f16693d
azenui25 <asanga.nicholas@yahoo.com>
2020-11-04 16:06:41 +0100

    finished error handling

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -28,7 +28,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                     modalStateErrors.push(error.error.erros[key])
                   }
                 }
-                throw modalStateErrors;
+                throw modalStateErrors.flat();
               } else {
                 this.toastr.error(error.statusText, error.status);
               }
